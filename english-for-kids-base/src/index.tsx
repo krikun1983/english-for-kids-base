361b7da1ad756ec8aede954076aae943b23963ac
Stanislav Akhmadeev <akhmadeevst@gmail.com>
2021-06-24 22:43:16 +0400

    feat: webpack setup for react

---
+++ english-for-kids-base/src/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.scss';
+import './heading.scss';
+import './content.scss';
+import car from './assets/car.jpg';
+
+const Heading = () => <h1 className="heading">My React and TypeScript App</h1>;
+
+const Content = () => (
+  <div>
+    <img src={car} className="car" />
+  </div>
+);
+
+const App = () => (
+  <>
+    <Heading />
+    <Content />
+  </>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
