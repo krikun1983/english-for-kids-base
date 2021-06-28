602ae75b6844d90588c24d278796f6073dd063d2
Stanislav Akhmadeev <akhmadeevst@gmail.com>
2021-06-28 22:27:12 +0400

    feat: add toggle-switch

--- english-for-kids-base/src/components/header/index.tsx
+++ english-for-kids-base/src/components/header/index.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import './header.scss';
+import React, { useState } from 'react';
 import Nav from './nav';
+import ToggleSwitch from './nav/toggle-switch';
+import './header.scss';
 
 const Header = (): JSX.Element => {
+  const [newsletter, setNewsletter] = useState<boolean>(false);
+
+  const onNewsletterChange = (
+    checked: boolean | ((prevState: boolean) => boolean),
+  ) => {
+    setNewsletter(checked);
+  };
+
   return (
     <header className="header">
       <Nav />
+      <ToggleSwitch
+        id="newsletter"
+        checked={newsletter}
+        onChange={onNewsletterChange}
+        name="toggleSwitch"
+        disabled={false}
+      />
     </header>
   );
 };
---
+++ english-for-kids-base/src/components/header/nav/toggle-switch/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import './toggle-switch.scss';
+
+interface InputCheckboxName {
+  id: string;
+  name: string;
+  checked: boolean;
+  onChange: (event: boolean) => void; // React.ChangeEvent<HTMLInputElement>
+  disabled: boolean;
+}
+
+const ToggleSwitch = ({
+  id,
+  name,
+  checked,
+  onChange,
+  disabled,
+}: InputCheckboxName): JSX.Element => {
+  return (
+    <div className="toggle-switch">
+      <input
+        type="checkbox"
+        className="toggle-switch-checkbox"
+        name={name}
+        id={id}
+        checked={checked}
+        disabled={disabled}
+        onChange={event => onChange(event.target.checked)}
+      />
+      {/* <label className="toggle-switch-label" htmlFor={id}>
+        <span
+          className={
+            disabled
+              ? 'toggle-switch-inner toggle-switch-disabled'
+              : 'toggle-switch-inner'
+          }
+          data-yes="Train"
+          data-no="Play"
+        />
+        <span
+          className={
+            disabled
+              ? 'toggle-switch-switch toggle-switch-disabled'
+              : 'toggle-switch-switch'
+          }
+        />
+      </label> */}
+      {id ? (
+        <label className="toggle-switch-label" htmlFor={id}>
+          <span
+            className={
+              disabled
+                ? 'toggle-switch-inner toggle-switch-disabled'
+                : 'toggle-switch-inner'
+            }
+            data-yes="Train"
+            data-no="Play"
+          />
+          <span
+            className={
+              disabled
+                ? 'toggle-switch-switch toggle-switch-disabled'
+                : 'toggle-switch-switch'
+            }
+          />
+        </label>
+      ) : null}
+    </div>
+  );
+};
+
+export default ToggleSwitch;
