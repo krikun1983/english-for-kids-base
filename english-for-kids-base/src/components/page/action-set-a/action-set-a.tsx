5e36d0534d001cf138313ae8d133c057d8f99e08
Stanislav Akhmadeev <akhmadeevst@gmail.com>
2021-06-29 18:58:02 +0400

    feat: add page action-set-a

--- english-for-kids-base/src/components/page/action-set-a/action-set-a.tsx
+++ english-for-kids-base/src/components/page/action-set-a/action-set-a.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import cards from '../../../cards';
+import CardPage from '../card-page';
+import './action-set-a.scss';
+
+export interface Cards {
+  id: number;
+  word: string;
+  translation: string;
+  image: string;
+  audioSrc: string;
+}
 
 const ActionSetA = (): JSX.Element => {
-  return <div>Страница ActionSetA</div>;
+  return (
+    <div className="main-wrapper">
+      {cards[0].map(card => {
+        const { id, word, translation, image, audioSrc }: Cards = card;
+        return (
+          <CardPage
+            key={id}
+            word={word}
+            translation={translation}
+            image={image}
+            audioSrc={audioSrc}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default ActionSetA;
